fef38c9561c26e24a29db5da2dea232bb128aec2
Faisal Chishti <faysalchishti@gmail.com>
2023-02-16 23:19:36 +0500

    adding logic to mark todo items as done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const todoList = [
     {
         id: 2,
         text: "Meeting with friends",
-        done: true
+        done: false
     },
     {
         id: 3,
@@ -33,7 +33,6 @@ function addTodoItem() {
 
 function handleAdd() {
     addTodoItem();
-    todoListContainer.innerHTML = '';
     printTodoList();
     todoInput.value = '';
 }
@@ -41,16 +40,30 @@ function handleAdd() {
 function createTodoItem(item) {
     const todoItem = document.createElement('div');
     todoItem.classList.add('item');
+    todoItem.setAttribute('data-id', item.id);
+
+    if (item.done) {
+        todoItem.classList.add('done');
+    }
 
     const todoText = document.createElement('div');
     todoText.classList.add('todo-text');
     todoText.innerText = item.text;
 
+    const todoStatus = document.createElement('div');
+    const checkIcon = document.createElement('i');
+
+    checkIcon.classList.add('fa-solid', 'fa-check');
+    todoStatus.classList.add('todo-status');
+
+    todoStatus.appendChild(checkIcon);
+    todoItem.appendChild(todoStatus);
     todoItem.appendChild(todoText);
     return todoItem;
 }
 
 function printTodoList() {
+    todoListContainer.innerHTML = '';
     todoList.forEach(function(entry) {
         todoListContainer.appendChild(createTodoItem(entry));
     });
@@ -58,4 +71,30 @@ function printTodoList() {
 
 
 todoAdd.addEventListener('click', handleAdd);
+
+// Add an event handler on todoListContainer 
+todoListContainer.addEventListener('click', function(e) {
+    let targ = e.target;
+    let todoItem = null;
+
+    while (!todoItem) {
+        if (targ.classList.contains('item')) {
+            todoItem = targ;
+        }
+        targ = targ.parentElement;
+    }
+
+
+    if (todoItem) {
+        const itemId = todoItem.getAttribute('data-id');
+        todoList.find(function(obj) {
+            if (obj.id == itemId) {
+                obj.done = true;
+                printTodoList();
+            }
+        });
+    
+    }
+});
+
 printTodoList();
