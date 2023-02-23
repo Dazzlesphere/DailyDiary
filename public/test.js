774f9333e258b2eeb7cc63aad43451deaa79890b
Faisal Chishti <faysalchishti@gmail.com>
2023-02-23 23:23:02 +0500

    test changes: nothing to do with the project

---
+++ public/test.js
@@ -0,0 +1,10 @@
+const some_val = 56;
+const another_val = 100;
+
+function sumNums(num1, num2) {
+    fetchsomethingfromserver()
+    console.log(num1 + num2);
+}
+
+sumNums(some_val, another_val);
+anotherfunction();
