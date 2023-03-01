a4f24b0a1baff0c4b66afeff473d4d6168dfacbe
Faisal Chishti <faysalchishti@gmail.com>
2023-03-01 22:37:35 +0500

    adding a login page

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 //import path from 'path';
-
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import connectDB from './config/db.js';
@@ -15,16 +13,23 @@ import helpers from './lib/helpers.js';
 // // Connect to the database
 // connectDB();
 
+const users = [
+    {
+        id: 1,
+        username: "f@something.com",
+        password: "abc",
+        name: "Some Name"
+    }
+]
+
 const app = express();
 const PORT = 3000;
 
-//app.use('/users', userRoutes);
-
 // Setup to accept json in bodyap
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 //app.use(express.json());
 
-// // Set the default view engine
+// Set the default view engine
 const hbs = createEngine({
     extname: '.hbs',
     defaultLayout: 'default',
@@ -39,15 +44,25 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 // Register the routes
-//app.get('/login', (req, res) => res.render("login", { pageTitle: "Get Going!!!", pageClass: "login-page" }))
+app.get('/login', (req, res) => res.render("login", { pageTitle: "Sign In With", pageClass: "auth-page login-page" }));
+app.get('/register', (req, res) => res.render("register", { pageTitle: "Signup", pageClass: "register-page" }));
+
+app.get('/authenticate', (req, res) => {
+    res.end();
+});
 
 app.post('/authenticate', (req, res) => {
-    console.log(req.body);
-    res.end();
-})
+    const authenticated = false;
+    const username = req.body.username;
+    const password = req.body.password;
 
-app.get('/login', (req, res) => {
-    res.render('login', { pageTitle: "Login" });
+    if (req.body.username && req.body.password) {
+        const user = users.find(obj => obj.username === username && obj.password === password);
+        if (user) {
+            return res.end('You are authenticated and your name is' + user.name);
+        }
+    }
+    res.end('username or password is incorrect');
 })
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
